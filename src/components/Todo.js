88d98553fdd828fd2a5701b418c7797f56375c0b
fots <60556553+fotscode@users.noreply.github.com>
2021-03-10 13:54:01 -0300

    del: deleted edit button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,7 @@
-import React, { useState } from "react";
-import TodoForm from "./TodoForm";
+import React from "react";
 import { BsTrash } from "react-icons/bs";
-import { MdModeEdit } from "react-icons/md";
 
 function Todo({ todos, completeTodo, removeTodo }) {
-    const [edit, setEdit] = useState({
-        id: null,
-        value: "",
-    });
-
-    console.log(todos);
-
     return todos.map((todo, index) => (
         <div
             className={todo.isComplete ? "todo-row complete" : "todo-row"}
@@ -24,7 +15,6 @@ function Todo({ todos, completeTodo, removeTodo }) {
                     className="delete-icon"
                     onClick={() => removeTodo(todo.id)}
                 />
-                <MdModeEdit className="edit-icon" />
             </div>
         </div>
     ));
