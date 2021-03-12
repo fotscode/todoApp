1f9a6cae9fec4575a02ff09c0fb20a2e9f302b77
fots <60556553+fotscode@users.noreply.github.com>
2021-03-12 16:05:33 -0300

    del: removed menu bar

--- public/electron.js
+++ public/electron.js
@@ -17,12 +17,8 @@ function createWindow() {
             ? "http://localhost:3000"
             : `file://${path.join(__dirname, "../build/index.html")}`
     );
-    if (isDev) {
-        // Open the DevTools.
-        //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-        mainWindow.webContents.openDevTools();
-    }
     mainWindow.on("closed", () => (mainWindow = null));
+    mainWindow.removeMenu();
 }
 
 app.on("ready", createWindow);
